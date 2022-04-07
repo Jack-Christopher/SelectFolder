aff5b5b2973439ba0f6c8523e6c6da7bc4eb0c75
Jack Christopher <jhuaihuah@unsa.edu.pe>
2022-04-07 16:14:57 -0500

    Navigation between folders of the same dir is available

--- sel.bat
+++ sel.bat
@@ -8,11 +8,32 @@ set idx=1
     echo.
     echo Current Directory: %cd%
 
-    call :print_dir
+    call :print_dir !idx! , n
+    choice /c ws /n
     
+    @REM if 'w' key is pressed
+    if %errorlevel%==1 (
+        if %idx% GTR 1 (
+            set /a idx-=1
+        )
+    )
+    @REM if 's' key is pressed
+    if %errorlevel%==2 (
+        if %idx% LSS !n! (
+            set /a idx+=1
+        )
+    )
+
     goto :loop
 
 :print_dir
+    set i=0
     for /d %%D in (*) do (
-        echo   %%~D
+        set /a i+=1
+        if %~1==!i! (
+            echo ^> %%~D
+        ) else (
+            echo   %%~D
+        )
     )
+    set %2=!i!
