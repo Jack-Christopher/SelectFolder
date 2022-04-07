d1a8ed4a83e712d4fd60a82d3bf631379d209438
Jack Christopher <jhuaihuah@unsa.edu.pe>
2022-04-07 16:09:00 -0500

    Function-oriented version

--- sel.bat
+++ sel.bat
@@ -2,13 +2,17 @@
 
 setlocal enableextensions enabledelayedexpansion
 
+set idx=1
 :loop
     cls
     echo.
     echo Current Directory: %cd%
 
+    call :print_dir
+    
+    goto :loop
+
+:print_dir
     for /d %%D in (*) do (
         echo   %%~D
     )
-        goto :loop
-    
